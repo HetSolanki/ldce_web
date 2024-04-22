6bec80b1d96de8374cc0eacc221d61a6d90e021e
Keval <kevalgajjar1311@gamil.com>
2024-04-22 17:12:30 +0530

    add components

---
+++ src/components/Sections/AcademicUnit.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import department from "../assets/department.png";
+import excellence from "../assets/excellence.png";
+import medal from "../assets/medal.png";
+
+export default function AcademicUnit() {
+    return (
+        <div className='flex bg-blue-100 justify-center py-10'>
+            <div className='bg-blue-100 w-11/12 md:w-4/5'>
+                <h3 className='text-3xl font-semibold mb-5 text-center sm:text-start'>ACADEMIC UNITS</h3>
+
+                <div className='flex flex-col items-center sm:flex-row justify-center sm:justify-between gap-10'>
+                    <div className='border-2 border-black transition duration-200 ease-in hover:ease-in text-sky-950 hover:bg-sky-950 hover:text-white w-4/6 sm:w-1/4 h-48 flex flex-col justify-center items-center cursor-pointer mb-4 md:mb-0'>
+                        <img className='w-3/12 sm:w-2/5' src={department} alt="" />
+                        <p className='text-lg md:text-xl lg:text-2xl font-semibold text-center'>DEPARTMENTS</p>
+                    </div>
+                    <div className='border-2 border-black transition duration-200 ease-in hover:ease-in text-sky-950 hover:bg-sky-950 hover:text-white w-4/6 sm:w-1/4 h-48 flex flex-col justify-center items-center cursor-pointer mb-4 md:mb-0'>
+                        <img className='w-3/12 sm:w-2/5' src={excellence} alt="" />
+                        <p className='text-lg md:text-xl lg:text-2xl font-semibold text-center'>CENTER OF EXCELLENCE</p>
+                    </div>
+                    <div className='border-2 border-black transition duration-200 ease-in hover:ease-in text-sky-950 hover:bg-sky-950 hover:text-white w-4/6 sm:w-1/4 h-48 flex flex-col justify-center items-center cursor-pointer'>
+                        <img className='w-3/12 sm:w-2/5' src={medal} alt="" />
+                        <p className='text-lg md:text-xl lg:text-2xl font-semibold text-center'>ACHIEVEMENTS</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
---
+++ src/components/Sections/Events.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import event from "../assets/event.jpg"
+
+export default function Events() {
+    return (
+        <div className='flex bg-blue-100 justify-center py-10'>
+            <div className='w-4/5'>
+
+                <div className='flex justify-center md:justify-normal items-end gap-5 mb-3'>
+                    <h3 className='text-3xl md:text-4xl text-red-600 font-semibold h-full mb-0'>EVENTS</h3>
+                    <button className='flex items-center justify-center bg-red-600 hover:bg-red-800 w-24 text-sm md:w-24 px-1 h-7 rounded text-white mb-1'>
+                        All Events
+                        <span class="material-symbols-outlined text-sm ps-1">
+                            arrow_forward_ios
+                        </span></button>
+                </div>
+
+                <div className='flex flex-wrap justify-center md:justify-between gap-10'>
+                    <div className='w-60 rounded-md overflow-hidden bg-white transition duration-200 ease-out hover:ease-in hover:scale-105'>
+                        <div className='h-60 overflow-hidden'>
+                            <img src={event} alt="" className='h-full transition duration-200 ease-out hover:ease-in hover:scale-105' />
+                        </div>
+                        <div className='p-3'>
+                            <p className='font-semibold text-gray-400'>16 Dec 2023</p>
+                            <p className=''>LDCE NSS and NCC in collaboration with CEE is organising Cycling Expedition 2024</p>
+                        </div>
+                    </div>
+                    <div className='w-60 rounded-md bg-white transition duration-200 ease-out hover:ease-in hover:scale-105'>
+                        <div className='h-60 overflow-hidden'>
+                            <img src={event} alt="" className='h-full transition duration-200 ease-out hover:ease-in hover:scale-105' />
+                        </div>
+                        <div className='p-3'>
+                            <p className='font-semibold text-gray-400'>16 Dec 2023</p>
+                            <p className=''>LDCE NSS and NCC in collaboration with CEE is organising Cycling Expedition 2024</p>
+                        </div>
+                    </div>
+                    <div className='w-60 rounded-md bg-white transition duration-200 ease-out hover:ease-in hover:scale-105'>
+                        <div className='h-60 overflow-hidden'>
+                            <img src={event} alt="" className='h-full transition duration-200 ease-out hover:ease-in hover:scale-105' />
+                        </div>
+                        <div className='p-3'>
+                            <p className='font-semibold text-gray-400'>16 Dec 2023</p>
+                            <p className=''>LDCE NSS and NCC in collaboration with CEE is organising Cycling Expedition 2024</p>
+                        </div>
+                    </div>
+                    <div className='w-60 rounded-md bg-white transition duration-200 ease-out hover:ease-in hover:scale-105'>
+                        <div className='h-60 overflow-hidden'>
+                            <img src={event} alt="" className='h-full transition duration-200 ease-out hover:ease-in hover:scale-105' />
+                        </div>
+                        <div className='p-3'>
+                            <p className='font-semibold text-gray-400'>16 Dec 2023</p>
+                            <p className=''>LDCE NSS and NCC in collaboration with CEE is organising Cycling Expedition 2024</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
